5e797d8bb62767e0de21dacddfd0bcc8b98e08d2
Nikita <nikperekrest@gmail.com>
2023-02-24 15:12:05 +0300

    almost completed site

--- myjavascript.js
+++ myjavascript.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
         $('.header_burger, nav').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $('.line#secret1').click(function(event) {
+        $('.line#secret1').toggleClass('active_line');
+    });
+    $('.line#secret2').click(function(event) {
+        $('.line#secret2').toggleClass('active_line');
+    });
 });
